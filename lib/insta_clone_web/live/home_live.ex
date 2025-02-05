b286e7074d9d37475fb605a3417fa12a3150775d
Lolo <louise.blanc@yahoo.se>
2025-02-05 19:23:30 +0100

    "posts"

--- lib/insta_clone_web/live/home_live.ex
+++ lib/insta_clone_web/live/home_live.ex
@@ -5,11 +5,25 @@ defmodule InstaCloneWeb.HomeLive do
   def render(assigns) do
     ~H"""
     <h1 class="text-2xl font-bold">Home</h1>
+
+    <.simple_form for{@form}>
+    <.input field={@form[:caption]} type="textarea" label="Caption" required />
+    </.simple_form>
+
     """
   end
 
   @impl true
   def mount(_params, _session, socket) do
+    form =
+      %Post{}
+      |> Post.changeset(%{})
+      |> to_form(as: "post")
+
+      socket =
+        socket
+        |> assign(form: form)
+
     {:ok, socket}
   end
 end
